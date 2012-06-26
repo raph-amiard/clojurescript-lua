a5068cf3f3b4f90080dd00154e8037a887ed8bec
Raphaël AMIARD <raph.amiard@gmail.com>
2012-06-26 17:58:04 +0200

    Using own lua-workbranch branch to provide clojurescript

--- script/init.sh
+++ script/init.sh
@@ -1,9 +1,10 @@
 echo "Getting clojurescript ..."
-git clone https://github.com/clojure/clojurescript.git clojurescript 2> /dev/null >&1
+git clone https://github.com/raph-amiard/clojurescript.git 2> /dev/null >&1
 
 echo "Compiling clojurescript into a jar .."
 cp script/clojurescript_project_file.clj clojurescript/project.clj
 cd clojurescript
+git checkout lua-workbranch
 lein jar 2> /dev/null > /dev/null
 
 echo "Cleaning up .."
