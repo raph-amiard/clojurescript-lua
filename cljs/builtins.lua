f36f879cda08f571aa9e6513caeb13e1d47b434d
Raphaël AMIARD <raph.amiard@gmail.com>
2012-07-10 14:54:27 +0200

    Adding a slice builtin equivalent to js slice

--- cljs/builtins.lua
+++ cljs/builtins.lua
@@ -118,3 +118,25 @@ function builtins.StringBuffer.new()
    function sb.toString(self, str)
    end
 end
+
+function table.slice (values,i1,i2)
+   local res = {}
+   local n = #values
+   -- default values for range
+   i1 = i1 or 1
+   i2 = i2 or n
+   if i2 < 0 then
+      i2 = n + i2 + 1
+   elseif i2 > n then
+      i2 = n
+   end
+   if i1 < 1 or i1 > n then
+      return {}
+   end
+   local k = 1
+   for i = i1,i2 do
+      res[k] = values[i]
+      k = k + 1
+   end
+   return res
+end
