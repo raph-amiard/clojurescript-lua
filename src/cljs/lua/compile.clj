a3a5d970660014e42da2cdaaa8eebc904bddf505
Raphaël AMIARD <raph.amiard@gmail.com>
2012-07-21 19:19:28 +0200

    Prototype compiler now working

--- src/cljs/lua/compile.clj
+++ src/cljs/lua/compile.clj
@@ -6,6 +6,7 @@
             [cljs.lua.common :as com])
   (:import  [java.io PrintWriter File FileInputStream FileOutputStream]))
 
+(def ^:dynamic *cljs-files*)
 
 (defn cljs-files-in
   "Return a sequence of all .cljs files in the given directory."
@@ -15,19 +16,50 @@
                   (not= \. (first name))))
           (file-seq dir)))
 
-(defn compile-dir [dir args]
-  (doseq [f (cljs-files-in dir)]
-    (doseq [form (cloader/make-forms-seq f)]
-      (comp/emit (ana/analyze (ana/empty-env) form)))))
+(defn ns-decl [file]
+  (let [first-form
+        (->> file
+             cloader/make-forms-seq
+             first
+             (ana/analyze (ana/empty-env)))]
+    (if (= :ns (:op first-form))
+      first-form
+      nil)))
 
-(defn compile-file [file args])
+(defn make-files-map [dir]
+  (apply hash-map (mapcat (fn [f] [((ns-decl f) :name) f]) (cljs-files-in dir))))
+
+(defn compile-seq [seq]
+  (doseq [form seq] (comp/emit (ana/analyze (ana/empty-env) form))))
+
+(defn compile-file [file]
+  (compile-seq (cloader/make-forms-seq file)))
+    
+
+(defn get-parent [file]
+  (.getParentFile (io/file (.getCanonicalPath file))))
+
+(defn compile-with-deps [file]
+  (let [nsdecl (ns-decl file)
+        requires (nsdecl :requires)]
+    (doseq [[ns-alias ns-name] requires]
+      (compile-with-deps (*cljs-files* ns-name)))
+    (compile-file file)))
+
+(defn compile-root-file [file]
+  (binding [*cljs-files* (make-files-map (get-parent file))]
+    (compile-with-deps file)))
+
+(defn compile [file args]
+  (let [nsd (ns-decl file)]
+    (compile-seq com/core-forms-seq)
+    ((if nsd compile-root-file compile-file) file)))
 
 (defn -main [args]
   (ana/with-core-macros "/cljs/lua/core"
     (binding [ana/*cljs-ns* 'cljs.user
               ana/*cljs-static-fns* true]
       (let [src-file (io/file (args :source))]
-        (println args)
         (if (.isDirectory src-file)
-          (compile-dir src-file args)
-          (compile-file src-file args))))))
+          (println "Input must be a cljsc file !")
+          (compile src-file args))))))
