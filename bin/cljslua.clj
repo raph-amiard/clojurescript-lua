8f50140ba51022eb5884f646c9038a2b989c9090
Raphaël AMIARD <raph.amiard@gmail.com>
2012-08-09 00:31:43 +0200

    init dirs

--- bin/cljslua.clj
+++ bin/cljslua.clj
@@ -1,6 +1,7 @@
 (require '[cljs.lua.repl :as repl])
 (require '[cljs.lua.config :as conf])
 (require '[cljs.lua.compile :as comp])
+(require '[cljs.lua.common :as com])
 (require '[cljs.analyzer :as ana])
 
 (def commands {"compile" comp/-main
@@ -15,6 +16,7 @@
         cmd-func (commands (first args))]
     (conf/load-config)
     (comp/compile-core)
+    (com/init-dirs)
     (if cmd-func
       (apply cmd-func (rest args))
       (println "Unknown command : " (first args)))))
